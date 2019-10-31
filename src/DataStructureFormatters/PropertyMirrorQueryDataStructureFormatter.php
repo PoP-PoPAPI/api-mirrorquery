dce71ffd3aca998907c0ded839c55d287fef1979
Leo <losoviz@gmail.com>
2019-10-31 12:09:58 +0800

    Print output as a properties file

---
+++ src/DataStructureFormatters/PropertyMirrorQueryDataStructureFormatter.php
@@ -0,0 +1,14 @@
+<?php
+namespace PoP\APIMirrorQuery\DataStructureFormatters;
+
+use PoP\ComponentModel\DataStructure\PropertyDataStructureFormatterTrait;
+
+class PropertyMirrorQueryDataStructureFormatter extends MirrorQueryDataStructureFormatter
+{
+    use PropertyDataStructureFormatterTrait;
+
+    public const NAME = 'mirrorquery-props';
+    public static function getName() {
+        return self::NAME;
+    }
+}
