43cbc2c76c55c2225b05eadae96c6cc2df86d533
Leo <losoviz@gmail.com>
2019-10-31 12:35:46 +0800

    Print output as XML

---
+++ src/DataStructureFormatters/XMLMirrorQueryDataStructureFormatter.php
@@ -0,0 +1,14 @@
+<?php
+namespace PoP\APIMirrorQuery\DataStructureFormatters;
+
+use PoP\ComponentModel\DataStructure\XMLDataStructureFormatterTrait;
+
+class XMLMirrorQueryDataStructureFormatter extends MirrorQueryDataStructureFormatter
+{
+    use XMLDataStructureFormatterTrait;
+
+    public const NAME = 'xml';
+    public static function getName() {
+        return self::NAME;
+    }
+}
