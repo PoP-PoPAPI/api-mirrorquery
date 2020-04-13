7a53a376686538c09533b15eaa0d7cb3623cc8a5
Leo <losoviz@gmail.com>
2020-04-13 19:07:52 +0800

    Added declare(strict_types=1);

--- src/Config/ServiceConfiguration.php
+++ src/Config/ServiceConfiguration.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace PoP\APIMirrorQuery\Config;
 
 use PoP\ComponentModel\Container\ContainerBuilderUtils;
--- src/DataStructureFormatters/XMLMirrorQueryDataStructureFormatter.php
+++ src/DataStructureFormatters/XMLMirrorQueryDataStructureFormatter.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace PoP\APIMirrorQuery\DataStructureFormatters;
 
 use PoP\ComponentModel\DataStructure\XMLDataStructureFormatterTrait;
