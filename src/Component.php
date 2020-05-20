48ba91b3aeed6722ad7f0fc9af37d707b4783ad5
Leonardo Losoviz <leo@getpop.org>
2020-05-20 18:55:18 +0800

    Fixed linting

--- src/Component.php
+++ src/Component.php
@@ -13,6 +13,8 @@ use PoP\Root\Component\YAMLServicesTrait;
  */
 class Component extends AbstractComponent
 {
+    use YAMLServicesTrait;
+    
     // const VERSION = '0.1.0';
 
     public static function getDependedComponentClasses(): array
@@ -21,7 +23,6 @@ class Component extends AbstractComponent
             \PoP\API\Component::class,
         ];
     }
-    use YAMLServicesTrait;
     /**
      * Initialize services
      */
